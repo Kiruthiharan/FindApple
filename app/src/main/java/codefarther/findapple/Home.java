4924e1dacff2415132f097c8a029749b77c31091
Kiruthiharan <kiruthiharan@gmail.com>
2018-09-24 17:55:38 +0530

    Splash Screen disabled and Navigation bar can be extended to other Activities

--- app/src/main/java/codefarther/findapple/Home.java
+++ app/src/main/java/codefarther/findapple/Home.java
@@ -1,6 +1,7 @@
 package codefarther.findapple;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,67 +15,28 @@ import android.support.v7.widget.Toolbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Home extends AppCompatActivity {
+public class Home extends baseactivity {
     TextView tvwelocme;
     String userName;
     private FirebaseAuth mAuth;
 
-    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        ConstraintLayout constraintLayout=(ConstraintLayout)findViewById(R.id.constraint_content);
+        getLayoutInflater().inflate(R.layout.activity_home, constraintLayout);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
         tvwelocme=(TextView)findViewById(R.id.tvwelcome);
-
-
-
-        mDrawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        // set item as selected to persist highlight
-                        menuItem.setChecked(true);
-                        // close drawer when item is tapped
-                        mDrawerLayout.closeDrawers();
-                        String s=menuItem.getTitle().toString();
-                        tvwelocme.setText(s);
-                        // Add code here to update the UI based on the item selected
-                        // For example, swap UI fragments here
-
-                        return true;
-                    }
-                });
 
         mAuth=FirebaseAuth.getInstance();
         FirebaseUser user=mAuth.getCurrentUser();
-
-
-
-
 
         //tvwelocme.setText("Welcome" + getIntent().getStringExtra("userName"));
         //tvwelocme.setText("welocme "+user.getEmail());
 
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
